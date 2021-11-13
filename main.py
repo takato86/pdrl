d172b6b0fbcfa4fe4a98495ffa7d921202e06d4b
Takato Okudo <okudo@nii.ac.jp>
2021-11-13 20:12:15 +0900

    working.

---
+++ main.py
@@ -0,0 +1,14 @@
+import json
+import gym
+import gym_m2s
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    with open("config.json", "r") as f:
+        configs = json.load(f)
+
+    main()
