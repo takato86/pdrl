f2b09098a1c38faebb17289d58dee76e1783a565
Takato Okudo <okudo@nii.ac.jp>
2022-07-15 18:20:46 +0900

    shaping for non-episodic task

--- pdrl/experiments/pick_and_place/is_success.py
+++ pdrl/experiments/pick_and_place/is_success.py
@@ -4,4 +4,5 @@ def is_success(done, info):
     # return done
     # if not done:
     #     return False
-    return bool(info["is_success"])
+    # return bool(info["is_success"])
+    return False
