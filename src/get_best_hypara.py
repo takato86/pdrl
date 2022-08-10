e997ec6609edde24d57198fa48a175afb1f836e0
Takato Okudo <okudo@nii.ac.jp>
2022-08-10 11:50:45 +0900

    get best hypara from db.

--- src/get_best_hypara.py
+++ src/get_best_hypara.py
@@ -6,9 +6,14 @@ def main():
         study_name="example-study",
         storage="mysql+pymysql://root:test@localhost/optunatest"
     )
-    print("Best value: {}".format(study.best_value))
-    print("Best parameters:")
-    print(study.best_params)
+    trials = study.get_trials()
+    sorted_trials = [trial for trial in trials if trial.value is not None]
+    sorted_trials = sorted(sorted_trials, key=lambda x: x.value, reverse=True)
+    for i, trial in enumerate(sorted_trials[:5]):
+        print(f"No {i + 1}:")
+        print(f"value: {trial.value}")
+        print("parameters: ")
+        print(trial.params)
 
 
 if __name__ == "__main__":
